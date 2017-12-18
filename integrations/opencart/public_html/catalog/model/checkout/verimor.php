05d9a3688502f8c9976dace1dd2af42ff12eef12
vasifhuseynov <31925263+vasifhuseynov@users.noreply.github.com>
2017-12-18 11:21:05 +0300

    Add files via upload

---
+++ integrations/opencart/public_html/catalog/model/checkout/verimor.php
@@ -0,0 +1,173 @@
+<?php
+class ModelCheckoutVerimor extends Model {
+    public function model_version(){
+        return "Model: v1.0.1";
+    }
+
+	public function get_order_by_telephone_number($telephone) {
+		$order_query = $this->db->query("SELECT *,os.name order_status FROM `" . DB_PREFIX . "order` o INNER JOIN `". DB_PREFIX . "order_status` os ON os.order_status_id = o.order_status_id WHERE o.telephone LIKE '%" . $telephone . "' order by date_added DESC Limit 1");
+
+
+		if ($order_query->num_rows) {
+		     return $order_query->row; 
+		} else {
+			return false;
+		}
+	}
+	
+	public function check_token($token){
+	    $query = $this->db->query("Select * from `" . DB_PREFIX . "api_session` a where a.token= '". $token ."'");
+	    
+	    if ($query->num_rows) {
+		    return true; 
+		} else {
+			return false;
+		}
+	}
+	
+	public function update_order_status($order_id,$status) {
+		$this->db->query("UPDATE `" . DB_PREFIX . "order` SET order_status_id =". (int)$status ."   WHERE order_id = '" . (int)$order_id . "'");
+		// Void the order first
+		$this->addOrderHistory($order_id, 0);
+	}   
+	
+	public function list_orders($date_added) {
+		$orders = $this->db->query("SELECT o.order_id,o.firstname,o.lastname,o.telephone,o.date_added,o.total,os.name as status FROM `" . DB_PREFIX . "order` o INNER JOIN `". DB_PREFIX . "order_status` os ON os.order_status_id = o.order_status_id  WHERE o.payment_method='Kapıda Ödeme' AND os.name='Onay Bekliyor' AND o.date_added > '" . $date_added . "' AND o.date_added >=Curdate() order by o.date_added ASC");
+        
+        $order_array = null;
+		if ($orders->num_rows) {
+		    foreach($orders->rows as $row){
+		        if ($order_array == null){
+		            $order_array = array($row);
+		        }else{
+		            array_push($order_array,$row);
+		        }
+		    }
+		     return $order_array; 
+		} else {
+			return false;
+		}
+	}	
+
+	public function addOrderHistory($order_id, $order_status_id, $comment = '', $notify = false, $override = false) {
+		$order_info = $this->getOrder($order_id);
+		
+		if ($order_info) {
+			// Fraud Detection
+			$this->load->model('account/customer');
+
+			$customer_info = $this->model_account_customer->getCustomer($order_info['customer_id']);
+
+			if ($customer_info && $customer_info['safe']) {
+				$safe = true;
+			} else {
+				$safe = false;
+			}
+
+			// Only do the fraud check if the customer is not on the safe list and the order status is changing into the complete or process order status
+			if (!$safe && !$override && in_array($order_status_id, array_merge($this->config->get('config_processing_status'), $this->config->get('config_complete_status')))) {
+				// Anti-Fraud
+				$this->load->model('setting/extension');
+
+				$extensions = $this->model_setting_extension->getExtensions('fraud');
+
+				foreach ($extensions as $extension) {
+					if ($this->config->get('fraud_' . $extension['code'] . '_status')) {
+						$this->load->model('extension/fraud/' . $extension['code']);
+
+						if (property_exists($this->{'model_extension_fraud_' . $extension['code']}, 'check')) {
+							$fraud_status_id = $this->{'model_extension_fraud_' . $extension['code']}->check($order_info);
+	
+							if ($fraud_status_id) {
+								$order_status_id = $fraud_status_id;
+							}
+						}
+					}
+				}
+			}
+
+			// If current order status is not processing or complete but new status is processing or complete then commence completing the order
+			if (!in_array($order_info['order_status_id'], array_merge($this->config->get('config_processing_status'), $this->config->get('config_complete_status'))) && in_array($order_status_id, array_merge($this->config->get('config_processing_status'), $this->config->get('config_complete_status')))) {
+				// Redeem coupon, vouchers and reward points
+				$order_totals = $this->getOrderTotals($order_id);
+
+				foreach ($order_totals as $order_total) {
+					$this->load->model('extension/total/' . $order_total['code']);
+
+					if (property_exists($this->{'model_extension_total_' . $order_total['code']}, 'confirm')) {
+						// Confirm coupon, vouchers and reward points
+						$fraud_status_id = $this->{'model_extension_total_' . $order_total['code']}->confirm($order_info, $order_total);
+						
+						// If the balance on the coupon, vouchers and reward points is not enough to cover the transaction or has already been used then the fraud order status is returned.
+						if ($fraud_status_id) {
+							$order_status_id = $fraud_status_id;
+						}
+					}
+				}
+
+				// Stock subtraction
+				$order_products = $this->getOrderProducts($order_id);
+
+				foreach ($order_products as $order_product) {
+					$this->db->query("UPDATE " . DB_PREFIX . "product SET quantity = (quantity - " . (int)$order_product['quantity'] . ") WHERE product_id = '" . (int)$order_product['product_id'] . "' AND subtract = '1'");
+
+					$order_options = $this->getOrderOptions($order_id, $order_product['order_product_id']);
+
+					foreach ($order_options as $order_option) {
+						$this->db->query("UPDATE " . DB_PREFIX . "product_option_value SET quantity = (quantity - " . (int)$order_product['quantity'] . ") WHERE product_option_value_id = '" . (int)$order_option['product_option_value_id'] . "' AND subtract = '1'");
+					}
+				}
+				
+				// Add commission if sale is linked to affiliate referral.
+				if ($order_info['affiliate_id'] && $this->config->get('config_affiliate_auto')) {
+					$this->load->model('account/customer');
+
+					if (!$this->model_account_customer->getTotalTransactionsByOrderId($order_id)) {
+						$this->model_account_customer->addTransaction($order_info['affiliate_id'], $this->language->get('text_order_id') . ' #' . $order_id, $order_info['commission'], $order_id);
+					}
+				}
+			}
+
+			// Update the DB with the new statuses
+			$this->db->query("UPDATE `" . DB_PREFIX . "order` SET order_status_id = '" . (int)$order_status_id . "', date_modified = NOW() WHERE order_id = '" . (int)$order_id . "'");
+
+			$this->db->query("INSERT INTO " . DB_PREFIX . "order_history SET order_id = '" . (int)$order_id . "', order_status_id = '" . (int)$order_status_id . "', notify = '" . (int)$notify . "', comment = '" . $this->db->escape($comment) . "', date_added = NOW()");
+
+			// If old order status is the processing or complete status but new status is not then commence restock, and remove coupon, voucher and reward history
+			if (in_array($order_info['order_status_id'], array_merge($this->config->get('config_processing_status'), $this->config->get('config_complete_status'))) && !in_array($order_status_id, array_merge($this->config->get('config_processing_status'), $this->config->get('config_complete_status')))) {
+				// Restock
+				$order_products = $this->getOrderProducts($order_id);
+
+				foreach($order_products as $order_product) {
+					$this->db->query("UPDATE `" . DB_PREFIX . "product` SET quantity = (quantity + " . (int)$order_product['quantity'] . ") WHERE product_id = '" . (int)$order_product['product_id'] . "' AND subtract = '1'");
+
+					$order_options = $this->getOrderOptions($order_id, $order_product['order_product_id']);
+
+					foreach ($order_options as $order_option) {
+						$this->db->query("UPDATE " . DB_PREFIX . "product_option_value SET quantity = (quantity + " . (int)$order_product['quantity'] . ") WHERE product_option_value_id = '" . (int)$order_option['product_option_value_id'] . "' AND subtract = '1'");
+					}
+				}
+
+				// Remove coupon, vouchers and reward points history
+				$order_totals = $this->getOrderTotals($order_id);
+				
+				foreach ($order_totals as $order_total) {
+					$this->load->model('extension/total/' . $order_total['code']);
+
+					if (property_exists($this->{'model_extension_total_' . $order_total['code']}, 'unconfirm')) {
+						$this->{'model_extension_total_' . $order_total['code']}->unconfirm($order_id);
+					}
+				}
+
+				// Remove commission if sale is linked to affiliate referral.
+				if ($order_info['affiliate_id']) {
+					$this->load->model('account/customer');
+					
+					$this->model_account_customer->deleteTransactionByOrderId($order_id);
+				}
+			}
+
+			$this->cache->delete('product');
+		}
+	}
+}	
